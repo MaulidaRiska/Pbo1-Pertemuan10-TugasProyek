4caeda866d987da31949967ea0f026349d5a2bc6
MaulidaRiska <maulidariska190@gmail.com>
2021-06-19 17:16:47 -0700

    Tugas Proyek

---
+++ src/Model/Dosen.java
@@ -0,0 +1,113 @@
+package Model;
+
+import java.time.*;
+//inheritance/sub-class
+public class Dosen extends Pegawai{
+    // atribut tambahan untuk dosen
+    private String nidn;
+    private String Jurusan;
+    private String BidangKeahlian;
+
+    // constructor
+    public Dosen(){
+        this.nidn = nidn;
+        this.Jurusan = Jurusan;
+        this.BidangKeahlian= BidangKeahlian;
+    }
+
+    public Dosen(String id, String nama, LocalDate tgllhr, String Alamat, String NoTelp, String Status, int gol, int LamaKerja,
+                 String nidn, String Jurusan, String BidangKeahlian){
+       setIdPegawai(id);
+       setNama(nama);
+       setTglLahir(tgllhr);
+       setAlamat(Alamat);
+       setNoTelp(NoTelp);
+       setStatus(Status);
+       setGol(gol);
+       setLamaKerja(LamaKerja);
+
+        this.nidn = nidn;
+        this.Jurusan = Jurusan;
+        this.BidangKeahlian= BidangKeahlian;
+
+    }
+
+    // getter dan setter
+    // method (3)
+    public String getNidn() {
+        return nidn;
+    }
+    // method (4)
+    public void setNidn(String nidn) {
+        this.nidn = nidn;
+    }
+    // method (5)
+    public String getJurusan() {
+        return Jurusan;
+    }
+    //method (6)
+    public void setJurusan(String jurusan) {
+        Jurusan = jurusan;
+    }
+    // method (7)
+    public String getBidangKeahlian() {
+        return BidangKeahlian;
+    }
+    //method (8)
+    public void setBidangKeahlian(String bidangKeahlian) {
+        BidangKeahlian = bidangKeahlian;
+    }
+
+    // method overriding (9)
+    public boolean thnks(String nama) {
+        return super.thnks(nama);
+    }
+    // method overriding (10)
+    public boolean user(String user) {
+        return super.user(user);
+    }
+
+    // Method menghitung tunjangan pegawai dosen (1)
+    long hitungTunjDosen(){
+        long tunjDosen = 1000000;
+        return tunjDosen;
+    }
+
+    // Method menghitung tunjangan fungsional dosen (2)
+    long hitungTunjFungsional(){
+        long tunjFungsional;
+        if (this.hitungUsia() < 40){
+            tunjFungsional = 1700000;
+        } else {
+            tunjFungsional = 2000000;
+        }
+
+        return tunjFungsional;
+    }
+
+
+    // cetak data pegawai, rincian gaji, total gaji
+    public void printPegawai(){
+        System.out.println("ID Pegawai       : " + this.getIdPegawai());
+        System.out.println("Nama Pegawai     : " + this.getNama());
+        System.out.println("Tgl Lahir        : " + this.getTglLahir());
+        System.out.println("Alamat           : " + this.getAlamat());
+        System.out.println("No Telpon        : " + this.getNoTelp());
+        System.out.println("Status           : " + this.getStatus());
+        System.out.println("Golongan         : " + this.getGol());
+        System.out.println("Lama Kerja       : " + this.getLamaKerja());
+        System.out.println("NIDN             : " + this.getNidn());
+        System.out.println("Jurusan          : " + this.getJurusan());
+        System.out.println("Bidang Keahlian  : " + this.getBidangKeahlian());
+        System.out.println("Usia             : " + this.hitungUsia() + " tahun");
+        System.out.println("Gaji Pokok       : Rp " + this.hitungGapok());
+        System.out.println("Bonus            : Rp " + this.hitungBonus());
+        System.out.println("Tunjangan Pegawai: Rp " + this.hitungTunj());
+        System.out.println("Tunjangan Dosen  : Rp " + this.hitungTunjDosen());
+        System.out.println("Tunjangan Fungsi : Rp " + this.hitungTunjFungsional());
+    }
+
+}
+
+
+
--- src/Model/Pegawai.java
+++ src/Model/Pegawai.java
@@ -2,28 +2,221 @@ package Model;
 
 import java.time.*;
 
+//parentclass/superclass
 public class Pegawai {
-
     // atribut
-    String idPegawai;
-    String nama;
-    LocalDate tglLahir;
-    String Alamat;
-    int NoTelp;
-    String Status;
-    int gol;
-    String AwalKerja;
+   private String idPegawai;
+   private String nama;
+   private LocalDate tglLahir;
+   private String Alamat;
+   private String NoTelp;
+   private String Status;
+   private int gol;
+   private int LamaKerja;
+   private String User;
+   private int Pass;
 
-    // constructor
-    Pegawai(String id, String nama,LocalDate tgllhr, String Alamat, int NoTelp, String Status, int gol, String AwalKerja){
-        this.idPegawai = id;
+    //getter dan setter
+    //method (6)
+    public String getIdPegawai() {
+        return idPegawai;
+    }
+    //method (7)
+    public void setIdPegawai(String idPegawai) {
+        this.idPegawai = idPegawai;
+    }
+    //method (8)
+    public String getNama() {
+        return nama;
+    }
+    //method (9)
+    public void setNama(String nama) {
         this.nama = nama;
-        this.tglLahir = tgllhr;
-        this.Alamat = Alamat;
-        this.NoTelp = NoTelp;
-        this.Status = Status;
+    }
+    //method (10)
+    public LocalDate getTglLahir() {
+        return tglLahir;
+    }
+    //method (11)
+    public void setTglLahir(LocalDate tglLahir) {
+        this.tglLahir = tglLahir;
+    }
+    //method (12)
+    public String getAlamat() {
+        return Alamat;
+    }
+    //method (13)
+    public void setAlamat(String alamat) {
+        Alamat = alamat;
+    }
+    //method (14)
+    public String getNoTelp() {
+        return NoTelp;
+    }
+    //method (15)
+    public void setNoTelp(String noTelp) {
+        NoTelp = noTelp;
+    }
+    //method (16)
+    public String getStatus() {
+        return Status;
+    }
+    //method (17)
+    public void setStatus(String status) {
+        Status = status;
+    }
+    //method (18)
+    public int getGol() {
+        return gol;
+    }
+    //method (19)
+    public void setGol(int gol) {
         this.gol = gol;
-        this.AwalKerja = AwalKerja;
+    }
+    //method (20)
+    public int getLamaKerja() {
+        return LamaKerja;
+    }
+    //method (21)
+    public void setLamaKerja(int lamaKerja) {
+        LamaKerja = lamaKerja;
+    }
+    //method (22)
+    public String getUser() {
+        return User;
+    }
+    //method (23)
+    public void setUser(String user) {
+        User = user;
+    }
+    //method (24)
+    public int getPass() {
+        return Pass;
+    }
+    //method (25)
+    public void setPass(int pass) {
+        Pass = pass;
+    }
+
+    // constructor (1)
+    public Pegawai(String idPegawai, String nama, LocalDate tglLahir, String alamat, String noTelp, String status, int gol, int lamaKerja) {
+        this.idPegawai = idPegawai;
+        this.nama = nama;
+        this.tglLahir = tglLahir;
+        this.Alamat = alamat;
+        this.NoTelp = noTelp;
+        this.Status = status;
+        this.gol = gol;
+        this.LamaKerja = lamaKerja;
+    }
+    // constructor (1)
+    public Pegawai() {
+        System.out.println("      Data Gaji Pegawai        ");
+        System.out.println("===============================");
+    }
+    // constructor (2)
+    public Pegawai(String User)
+    {
+        this.User = User;
+        System.out.println("===============================");
+        System.out.println("Username: "+this.User);
+        System.out.println("===============================");
+    }
+    // construktor (3)
+
+    public Pegawai(int pass) {
+        Pass = pass;
+        System.out.println("Password: "+this.Pass);
+        System.out.println("===============================");
+    }
+
+    // method overloading (26)
+    public boolean thnks(String nama)
+    {
+        String thnks ="Trimakasih Sudah berkerja disini, "+this.nama;
+        System.out.println(thnks);
+        return false;
+    }
+    // method overloading (26)
+    public boolean user(String user)
+    {
+        String thnks ="Username, "+this.User;
+        System.out.println(user);
+        return false;
+    }
+
+    // method menghitung usia pegawai (1)
+    public int hitungUsia(){
+        // mendapatkan tanggal hari ini
+        LocalDate today = LocalDate.now();
+        // hitung selisih tgl lahir dan tgl hari ini
+        Period selisih = Period.between(this.tglLahir, today);
+        return selisih.getYears();
+    }
+
+    //method hitung gaji pokok dari gol dan umur (2)
+    public long hitungGapok(){
+        long gapok = 0;
+        if (this.gol == 1){
+            if (this.hitungUsia() < 40){
+                gapok = 3500000;
+            } else {
+                gapok = 4500000;
+            }
+        } else if (this.gol == 2){
+            if (this.hitungUsia() < 40){
+                gapok = 5000000;
+            } else {
+                gapok = 6000000;
+            }
+        }
+        return gapok;
+    }
+    // method hitung bonus dari lama kerja (3)
+    public long hitungBonus() {
+        long bonus =0;
+        if (this.LamaKerja >= 6) {
+            bonus = 500000;
+        } else {
+            bonus = 0;
+        }
+        return bonus;
+    }
+    // method hitung Tunjangan dari Status (4)
+    public long hitungTunj() {
+        long tunj =0;
+        if (this.Status == "Menikah") {
+            tunj = 50000;
+        } else {
+            tunj = 0;
+        }
+        return tunj;
+    }
+    // method hitung gaji bersih (5)
+    public long HitungGaber(){
+        long sum = this.hitungGapok() + this.hitungBonus() + this.hitungTunj();
+        return sum;
+    }
+
+    // mencetak data pegawai dan gaji pokoknya
+    public void printPegawai(){
+        System.out.println("ID Pegawai   : " + this.idPegawai);
+        System.out.println("Nama Pegawai : " + this.nama);
+        System.out.println("Tgl Lahir    : " + this.tglLahir);
+        System.out.println("Alamat       : " + this.Alamat );
+        System.out.println("No Telpon    : " + this.NoTelp );
+        System.out.println("Status       : " + this.Status);
+        System.out.println("Golongan     : " + this.gol);
+        System.out.println("Lama Kerja   : " + this.LamaKerja);
+        System.out.println("Gaji Pokok   : Rp " + this.hitungGapok());
+        System.out.println("Bonus        : Rp " + this.hitungBonus());
+        System.out.println("Tunjangan    : Rp " + this.hitungTunj());
+        System.out.println("Gaji Bersih  : Rp " + this.HitungGaber());
+        System.out.println("");
+
     }
 
 }
+
+
+
